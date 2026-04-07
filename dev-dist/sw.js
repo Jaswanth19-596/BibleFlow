e1d30d7afccee36c3180b3a0c0d61c04f897263a
Jaswanth <madhajaswanth@gmail.com>
2026-04-07 06:11:38 -0500

    Fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0nh5ftjb10g"
+    "revision": "0.p065594bneg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
